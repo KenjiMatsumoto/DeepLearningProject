1d524cc92ef82d6b6654317a16e12d7377d2dd3b
KenjiMatsumoto <kenjimatsumoto1983@gmail.com>
2018-12-13 13:04:27 +0900

    modify CNNByKeras
    エラー修正し、モデルの作成まで完了

--- CNNByKeras.py
+++ CNNByKeras.py
@@ -1,18 +1,17 @@
 
 # coding: utf-8
 
-# In[6]:
+# In[21]:
 
 
 # import 
 from keras.datasets import mnist
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Input, Dense, Flatten
 
 
-# In[7]:
+# In[22]:
 
 
 # mnistデータの取得
@@ -20,24 +19,23 @@ from keras.layers import Conv2D, MaxPooling2D
 x_train1, x_vaild, y_train1, y_valid = train_test_split(x_train, y_train, test_size=0.175)
 
 
-# In[9]:
+# In[25]:
 
 
 # model作成 CNNByChainerと同じ層構成にする
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='sigmoid', input_shape=(28, 28, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='sigmoid'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, (3, 3), activation='sigmoid'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, (3, 3), activation='sigmoid'))
-# model.add(Dropout(0.25))
+input = Input((28, 28, 1))
+# 畳み込み層の実装
+cnn = Conv2D(32, kernel_size=(3, 3), padding='same', activation='sigmoid', input_shape=(28, 28, 1))(input)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(1, 1))(cnn)
+cnn = Conv2D(64, (3, 3), padding='same', activation='sigmoid')(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(1, 1))(cnn)
+cnn = Conv2D(128, (3, 3), padding='same', activation='sigmoid')(cnn)
+cnn = MaxPooling2D(pool_size=(2, 2), strides=(1, 1))(cnn)
+cnn = Conv2D(128, (3, 3), padding='same', activation='sigmoid')(cnn)
 # 入力を平滑化する層（いわゆるデータをフラット化する層、例えば4次元配列を1次元配列に変換するなど）
-model.add(Flatten())
+fc = Flatten()(cnn)
 # denseは全結合層
-model.add(Dense(1000, activation='sigmoid'))
-# dropout
-# model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
+fc = Dense(1000, activation='sigmoid')(fc)
+softmax = Dense(10, activation='softmax')(fc)
+model = Model(input=input, output=softmax)
 
