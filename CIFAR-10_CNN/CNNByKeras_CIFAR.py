9522f5eb2df58575d25ca6d0b783a18352eb4203
kenji matsumoto <kenjimatsumoto1983@gmail.com>
2019-01-05 09:06:28 +0900

    update ファイルの更新
    ResNet追加Versionをhtmlとpyファイルに反映

--- CIFAR-10_CNN/CNNByKeras_CIFAR.py
+++ CIFAR-10_CNN/CNNByKeras_CIFAR.py
@@ -8,7 +8,7 @@
 from keras.datasets import cifar10
 from sklearn.model_selection import train_test_split
 from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Input, Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Dropout, Input, Dense, Flatten, BatchNormalization, Activation, add
 from keras.optimizers import Adam
 from keras.losses import categorical_crossentropy
 from keras.utils import to_categorical
@@ -19,40 +19,64 @@ import numpy as np
 # In[ ]:
 
 
-# model作成 CNNByChainerと同じ層構成にする
-def create_CNN_model(input_shape=(32, 32, 3), class_num=10):
-    input = Input((32, 32, 3))
-    kernel_size = (3, 3)
-    max_pool_size = (2, 2)
-    # 畳み込み層の実装
-    # 1層目
-    cnn = Conv2D(32, kernel_size=kernel_size, padding='same', strides=(1, 1), activation='relu', input_shape=(32, 32, 3))(input)
-    cnn = MaxPooling2D(pool_size=max_pool_size, strides=(2, 2))(cnn)
-    cnn = Conv2D(64, kernel_size, padding='same', strides=(1, 1), activation='relu')(cnn)
-    cnn = MaxPooling2D(pool_size=max_pool_size, strides=(2, 2))(cnn)
-    cnn = Dropout(0.25)(cnn)
-    cnn = Conv2D(128, kernel_size, padding='same', strides=(1, 1), activation='relu')(cnn)
-    cnn = MaxPooling2D(pool_size=max_pool_size, strides=(2, 2))(cnn)
-    cnn = Conv2D(256, kernel_size, padding='same', strides=(1, 1), activation='relu')(cnn)
-    cnn = MaxPooling2D(pool_size=max_pool_size, strides=(2, 2))(cnn)
-    cnn = Dropout(0.25)(cnn)
-    cnn = Conv2D(256, kernel_size, padding='same', strides=(1, 1), activation='relu')(cnn)
-    # 入力を平滑化する層（いわゆるデータをフラット化する層、例えば4次元配列を1次元配列に変換するなど）
-    fc = Flatten()(cnn)
-    # denseは全結合層
-    fc = Dense(1000, activation='relu')(fc)
-    fc = Dropout(0.5)(fc)
-    softmax = Dense(10, activation='softmax')(fc)
-    model = Model(input=input, output=softmax)
+def rescell(data, filters, kernel_size, option=False):
+    strides=(1,1)
+    if option:
+        strides=(2,2)
+    x=Conv2D(filters=filters,kernel_size=kernel_size,strides=strides,padding="same")(data)
+    x=BatchNormalization()(x)
+    x=Activation('relu')(x)
     
-    return model
+    data=Conv2D(filters=int(x.shape[3]), kernel_size=(1,1), strides=strides, padding="same")(data)
+    x=Conv2D(filters=filters,kernel_size=kernel_size,strides=(1,1),padding="same")(x)
+    x=BatchNormalization()(x)
+    x=add([x,data])
+    x=Activation('relu')(x)
+    return x
+
+def ResNet(img_rows, img_cols, img_channels, x_train):
+	input=Input(shape=(img_rows,img_cols,img_channels))
+	x=Conv2D(32,(7,7), padding="same", input_shape=x_train.shape[1:],activation="relu")(input)
+	x=MaxPooling2D(pool_size=(2,2))(x)
+
+	x=rescell(x,64,(3,3))
+	x=rescell(x,64,(3,3))
+	x=rescell(x,64,(3,3))
+
+	x=rescell(x,128,(3,3),True)
+
+	x=rescell(x,128,(3,3))
+	x=rescell(x,128,(3,3))
+	x=rescell(x,128,(3,3))
+
+	x=rescell(x,256,(3,3),True)
+
+	x=rescell(x,256,(3,3))
+	x=rescell(x,256,(3,3))
+	x=rescell(x,256,(3,3))
+	x=rescell(x,256,(3,3))
+	x=rescell(x,256,(3,3))
+
+	x=rescell(x,512,(3,3),True)
+
+	x=rescell(x,512,(3,3))
+	x=rescell(x,512,(3,3))
+
+	x=AveragePooling2D(pool_size=(int(x.shape[1]),int(x.shape[2])),strides=(2,2))(x)
+
+	x=Flatten()(x)
+	x=Dense(units=10,kernel_initializer="he_normal",activation="softmax")(x)
+	model=Model(inputs=input,outputs=[x])
+	return model
 
 def train():
 
-    model = create_CNN_model()
+
     # cifar10のデータ取得
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_train1, x_valid, y_train1, y_valid = train_test_split(x_train, y_train, test_size=0.2)
+
+    model = ResNet(32,32,3,x_train)
     # RGB画像で32×32なので32×32×3にreshapeする
     x_train = x_train.reshape(x_train.shape[0], 32, 32, 3)
     x_valid = x_valid.reshape(x_valid.shape[0], 32, 32, 3)
@@ -80,7 +104,7 @@ def train():
     print('Test accuracy:', score[1])
 
 
-# In[5]:
+# In[3]:
 
 
 train()
